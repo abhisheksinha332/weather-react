40ac44e561d9acedfc4114b64261927d5196b0c5
Abhishek Sinha <3327abhishek@gmail.com>
2020-12-21 22:24:58 +0530

    Initial react setup

---
+++ src/component/weather.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Weather = () => {
+    return(
+        <div className="container">
+            <div className="cards">
+                <h1>India</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Weather;
